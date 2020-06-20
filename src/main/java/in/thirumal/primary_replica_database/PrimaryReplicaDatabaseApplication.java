3720e2f2653fc5b426f86c048072a70dfdf3bc77
Thirumal <m.thirumal@hotmail.com>
2020-06-20 13:20:59 +0530

    Added spring boot project

---
+++ src/main/java/in/thirumal/primary_replica_database/PrimaryReplicaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal.primary_replica_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryReplicaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryReplicaDatabaseApplication.class, args);
+	}
+
+}
